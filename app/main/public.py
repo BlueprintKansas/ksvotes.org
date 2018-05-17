32a210c333f5488b4097efee88904f489a5074e0
Joshua Mitchell <joshua@gpsimpact.com>
2018-05-17 17:30:33 -0400

    adding basic templating

--- app/main/public.py
+++ app/main/public.py
@@ -4,4 +4,4 @@ from flask import g, url_for, render_template
 
 @main.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html')
--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -4,3 +4,7 @@ def test_app(app, session, client):
 def test_homepage(app, session, client):
     response = client.get('/')
     assert response.status_code == 200
+
+def test_404(app, session, client):
+    response = client.get('/asdfasdfadcasdcmasdocasdf')
+    assert response.status_code == 404
