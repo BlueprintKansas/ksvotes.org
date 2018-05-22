a14c8eada3f986222c44c769f0bf412add9b86f7
Joshua Mitchell <joshua@gpsimpact.com>
2018-05-22 13:54:55 -0500

    adding the files because git hurts my brain

---
+++ app/models/clerk.py
@@ -0,0 +1,19 @@
+from app import db
+from datetime import datetime
+
+class Clerks(db.Model):
+    __tablename__ = "clerks"
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow())
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow())
+    county = db.Column(db.String()) #enum of ks counties
+    officer = db.Column(db.String())
+    email = db.Column(db.String())
+    phone = db.Column(db.String()) #formatted NNNNNNNNNN
+    fax = db.Column(db.String()) #formatted NNNNNNNNNN
+    address1 = db.Column(db.String())
+    address2 = db.Column(db.String())
+    city = db.Column(db.String())
+    state = db.Column(db.String(), default='KS')
+    zip = db.Column(db.String())
