d3ef726abd043a7794253b44e77a1c1bf162a895
Joshua Mitchell <joshua@gpsimpact.com>
2018-05-29 10:58:59 -0500

    reorganizing scss tree, basic material design / bootstrap forms

--- app/main/public.py
+++ app/main/public.py
@@ -2,9 +2,13 @@ from app.main import main
 from flask import g, url_for, render_template
 
 #step 0 / 0x
+
+
 @main.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    counties = ["Allen","Anderson","Atchison","Barber","Barton","Bourbon","Brown","Butler","Chase","Chautauqua","Cherokee","Cheyenne","Clark","Clay","Cloud","Coffey","Comanche","Cowley","Crawford","Decatur","Dickinson","Doniphan","Douglas","Edwards","Elk","Ellis","Ellsworth","Finney","Ford","Franklin","Geary","Gove","Graham","Grant","Gray","Greeley","Greenwood","Hamilton","Harper","Harvey","Haskell","Hodgeman","Jackson","Jefferson","Jewell","Johnson","Kearny","Kingman","Kiowa","Labette","Lane","Leavenworth","Lincoln","Linn","Logan","Lyon","Marion","Marshall","McPherson","Meade","Miami","Mitchell","Montgomery","Morris","Morton","Nemaha","Neosho","Ness","Norton","Osage","Osborne","Ottawa","Pawnee","Phillips","Pottawatomie","Pratt","Rawlins","Reno","Republic","Rice","Riley","Rooks","Rush","Russell","Saline","Scott","Sedgwick","Seward","Shawnee","Sheridan","Sherman","Smith","Stafford","Stanton","Stevens","Sumner","Thomas","Trego","Wabaunsee","Wallace","Washington","Wichita","Wilson","Woodson","Wyandotte"]
+
+    return render_template('index.html', counties=counties)
 
 #step 1
 @main.route('/citizenship')
