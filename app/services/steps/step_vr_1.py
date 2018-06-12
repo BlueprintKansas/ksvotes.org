b3abe7516aa8ced1558a40cfd024cbf4bdc55e7b
Joshua Mitchell <joshua@gpsimpact.com>
2018-06-12 13:36:34 -0500

    moving some rep logic into function on step

--- app/services/steps/step_vr_1.py
+++ app/services/steps/step_vr_1.py
@@ -4,17 +4,17 @@ class Step_VR_1(Step):
 	form_requirements = ['citizen']
 	step_requirements = []
 	endpoint = '/vr/citizenship'
-	# prev_step = Step_0()
+	prev_step = 'Step_0'
 	next_step = None
 
-	def all_requirements(self):
-		return self.form_requirements + self.step_requirements
 
-    #this feels really redundant to me as the form should already require these to be in the payload before submitting.
-	def validate(self):
+	def run(self):
 		if self.is_complete:
 			return True
-		if not all(k in self.form_payload for k in self.form_requirements):
+
+		if not self.verify_form_requirements():
 			return False
+
 		self.is_complete = True
 		self.next_step = 'Step_VR_2'
+		return True
