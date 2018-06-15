74dc39a9803af5b3ba3d2235bcaa4452396ef647
Joshua Mitchell <joshua@gpsimpact.com>
2018-06-14 19:33:55 -0500

    need to come back to this on usps

--- app/services/steps/step_vr_4.py
+++ app/services/steps/step_vr_4.py
@@ -1,10 +1,4 @@
 from app.services.steps import Step
 
 class Step_VR_4(Step):
-
-    #this feels really redundant to me as the form should already require these to be in the payload before submitting.
-	def is_complete(self):
-		return False
-
-	def next_step(self):
-		return '/'
+	endpoint = '/vr/party'
