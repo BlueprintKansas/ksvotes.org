30dc6bfd7ed9c7dc6470a945cee8d25e7a83ab74
Joshua Mitchell <JoshuaMitchell@users.noreply.github.com>
2018-06-18 13:26:21 -0500

    tests/logic vr 4 (#53)

--- app/main/VR/VR4_party.py
+++ app/main/VR/VR4_party.py
@@ -1,12 +1,22 @@
 from app.main import main
 from flask import g, url_for, render_template, redirect, request
 from app.main.forms import FormVR4
+from app import db
 from app.models import Registrant
 from app.decorators import InSession
-
+from app.services import SessionManager
+from app.services.steps import Step_VR_4
 @main.route('/vr/party', methods=["GET", "POST"])
+@InSession
 def vr4_party():
-    form = FormVR4()
+    form = FormVR4(
+        party = g.registrant.party,
+    )
     if request.method == "POST" and form.validate_on_submit():
-        return jsonify({"post": "success"})
+        step = Step_VR_4(form.data)
+        step.run()
+        g.registrant.update(form.data)
+        db.session.commit()
+        session_manager = SessionManager(g.registrant,step)
+        return redirect(session_manager.get_redirect_url())
     return render_template('vr/party.html', form=form)
