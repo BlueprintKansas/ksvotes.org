26370b51727e956fa5425d0f0e0793d0ed4f882b
Peter Karman <karpet@peknet.com>
2018-06-21 23:40:17 -0500

    Zap dupe import

--- app/main/forms/vr_1.py
+++ app/main/forms/vr_1.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField
 from wtforms.validators import DataRequired
-from flask_babel import lazy_gettext, lazy_gettext
+from flask_babel import lazy_gettext
 
 class FormVR1(FlaskForm):
     is_citizen = BooleanField(lazy_gettext('1VR_citizen'), validators=[DataRequired(message=lazy_gettext('Required'))])
