8c1650da8b0216b6ef2b6f1f9bbf262677140011
Joshua Mitchell <joshua@gpsimpact.com>
2018-07-01 12:35:20 -0700

    for rebase

--- app/main/VR/__init__.py
+++ app/main/VR/__init__.py
@@ -3,3 +3,4 @@ from .VR2_name import vr2_name
 from .VR3_address import vr3_address
 from .VR4_party import vr4_party
 from .VR5_identification import vr5_identification
+from .VR6_preview_sign import vr6_preview_sign
