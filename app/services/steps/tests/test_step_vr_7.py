89ffd6ff7e24a48a11dd84387e08ff00f057d891
Peter Karman <karpet@peknet.com>
2018-07-10 09:15:29 -0500

    Get tests passing

--- app/services/steps/tests/test_step_vr_7.py
+++ app/services/steps/tests/test_step_vr_7.py
@@ -2,26 +2,18 @@ from app.models import *
 from app.services.steps import Step_VR_7
 
 def test_step_vr7_is_complete_false(app, db_session, client):
-    """
-        Verify that this registrant is not ready to move on to the next step.
-
-    """
     form_payload = {}
-    step = Step_VR_5(form_payload)
+    step = Step_VR_7(form_payload)
     assert step.run() == False
     assert step.is_complete == False
     assert step.next_step == None
 
 
 def test_step_vr7_is_complete_true(app, db_session, client):
-    """
-        Verify that this registrant is not ready to move on to the next step and next step is VR 8.
-
-    """
     form_payload = {
-        "affiramtion": True
+        "affirmation": True
     }
-    step = Step_VR_5(form_payload)
+    step = Step_VR_7(form_payload)
     assert step.run() == True
     assert step.is_complete == True
-    assert step.next_step == 'Step_VR_7'
+    assert step.next_step == 'Step_VR_8'
