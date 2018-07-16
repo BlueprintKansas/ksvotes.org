6c8856d217a42d063549180b2ff3b811ab83a70b
Peter Karman <karpet@peknet.com>
2018-07-15 23:37:25 -0500

    Fix/nav translations (#145)
    * Advance not Advanced Ballot
    
    * whitespace clean up (maybe fixing bug with preassigning party?)

--- app/main/forms/vr_4.py
+++ app/main/forms/vr_4.py
@@ -5,12 +5,12 @@ from flask_babel import lazy_gettext
 
 class FormVR4(FlaskForm):
 	party = SelectField(lazy_gettext(u'4_party'),
-                            validators=[DataRequired(message=lazy_gettext(u'Required'))],
-							 choices=[
-								 ('', ''),
-								 ('democratic', 'Democratic'),
-								 ('republican', 'Republican'),
-								 ('unafilliated', 'Unafilliated'),
-								 ('libertarian', 'Libertarian'),
-							 ]
-						)
+		validators=[DataRequired(message=lazy_gettext(u'Required'))],
+		choices=[
+			 ('', ''),
+			 ('democratic', 'Democratic'),
+			 ('republican', 'Republican'),
+			 ('unafilliated', 'Unafilliated'),
+			 ('libertarian', 'Libertarian'),
+		]
+	)
