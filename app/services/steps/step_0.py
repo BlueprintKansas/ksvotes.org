38cc8f7b53f3c5fd520d96c2a05b6c4d27ee0cb3
Peter Karman <karpet@peknet.com>
2018-07-16 13:13:21 -0500

    Refactor SOS VV lookup, storing in db. TODO optional County flow

--- app/services/steps/step_0.py
+++ app/services/steps/step_0.py
@@ -18,7 +18,12 @@ class Step_0(Step):
         if not self.verify_form_requirements():
             return False
 
-        self.reg_found = self.lookup_registration(name_first=self.form_payload['name_first'], name_last=self.form_payload['name_last'], dob=self.form_payload['dob'], county=self.form_payload['county'])
+        self.reg_found = self.lookup_registration(
+            name_first=self.form_payload['name_first'],
+            name_last=self.form_payload['name_last'],
+            dob=self.form_payload['dob'],
+            county=self.form_payload['county']
+        )
         self.is_complete = True
         self.reg_lookup_complete = True
 
@@ -26,7 +31,7 @@ class Step_0(Step):
             self.next_step = 'Step_1'
             return True
 
-        self.next_step = 'Step_VR_1'
+        self.next_step = 'Step_1'
         return True
 
     def lookup_registration(self, name_first, name_last, dob, county):
@@ -36,12 +41,12 @@ class Step_0(Step):
         dob = dob.split('/')
         formatted_dob = "{year}-{month}-{day}".format(year=dob[2], month=dob[0], day=dob[1])
         request = kmvi.lookup(
-                        first_name = name_first,
-                        last_name = name_last,
-                        dob = formatted_dob,
-                        county = county
-                )
+            first_name = name_first,
+            last_name = name_last,
+            dob = formatted_dob,
+            county = county
+        )
         if request:
-            return request
+            return request.parsed()
 
         return False
