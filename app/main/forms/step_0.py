20f2c5eac276c38e0ea2dcb97c24601b5f520973
Peter Karman <karpet@peknet.com>
2018-08-09 06:09:53 -0500

    Use HTML5 email and telephone fields. Date field control is not ideal for DOB for different browsers

--- app/main/forms/step_0.py
+++ app/main/forms/step_0.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, SelectField, HiddenField
 from wtforms.validators import DataRequired, Email, Regexp, Optional
+from wtforms.fields.html5 import EmailField, TelField
 from flask_babel import lazy_gettext
 from dateutil.relativedelta import relativedelta
 import datetime
@@ -12,16 +13,22 @@ class FormStep0(FlaskForm):
     ref = HiddenField()
     name_first = StringField(lazy_gettext(u'0_first'), validators=[DataRequired(message=lazy_gettext(u'Required'))])
     name_last = StringField(lazy_gettext(u'0_last'), validators=[DataRequired(message=lazy_gettext(u'Required'))])
-    dob = StringField(lazy_gettext(u'0_dob'), validators=[DataRequired(message=lazy_gettext(u'Required')), Regexp('^\d{2}[\/\-]?\d{2}[\/\-]?\d{4}$', message=lazy_gettext(u'0_dob_flag'))])
+    dob = StringField(
+        lazy_gettext(u'0_dob'),
+        validators=[
+            DataRequired(message=lazy_gettext(u'Required')),
+            Regexp('^\d{2}[\/\-]?\d{2}[\/\-]?\d{4}$', message=lazy_gettext(u'0_dob_flag'))
+        ]
+    )
     county = SelectField(lazy_gettext(u'0_county'),
         validators=[Optional()],
         choices=construct_county_choices(lazy_gettext(u'0_county'))
     )
-    email = StringField(lazy_gettext(u'0_email'),
+    email = EmailField(lazy_gettext(u'0_email'),
         validators=[DataRequired(message=lazy_gettext(u'Required')),
         Email(message=lazy_gettext(u'0_email_flag'))]
     )
-    phone = StringField(lazy_gettext(u'0_phone'),
+    phone = TelField(lazy_gettext(u'0_phone'),
         validators=[Optional(), Regexp('^\d{3}[\-\.]?\d{3}[\-\.]?\d{4}$', message=lazy_gettext(u'0_phone_help'))]
     )
 
