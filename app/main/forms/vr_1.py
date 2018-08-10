8e6b56bdc9aedefccbe96b36066bdcd9ff034f8c
Peter Karman <karpet@peknet.com>
2018-08-10 08:32:17 -0500

    create custom RadioBooleanField class to act like Boolean, look like Radio

--- app/main/forms/vr_1.py
+++ app/main/forms/vr_1.py
@@ -1,14 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, RadioField
+from wtforms import BooleanField, RadioField, widgets
 from wtforms.validators import DataRequired
 from flask_babel import lazy_gettext
 
+class RadioBooleanField(RadioField):
+    widget = widgets.ListWidget()
+    option_widget = widgets.RadioInput()
+
+    def process_formdata(self, valuelist):
+        if 'False' in valuelist:
+            self.data = False
+        elif 'True' in valuelist:
+            self.data = True
+        else:
+            self.data = False
+
+
 class FormVR1(FlaskForm):
-    is_citizen = RadioField(
+    is_citizen = RadioBooleanField(
         lazy_gettext(u'1VR_citizen'),
-        choices=[('no', lazy_gettext(u'choice_no')), ('yes', lazy_gettext(u'choice_yes'))],
-        default='no', # always require user action
+        choices=[(False, lazy_gettext(u'choice_no')), (True, lazy_gettext(u'choice_yes'))],
+        default=False, # always require user action TODO broken
         validators=[DataRequired(message=lazy_gettext(u'Required'))]
     )
-    #is_citizen = BooleanField(lazy_gettext(u'1VR_citizen'), validators=[DataRequired(message=lazy_gettext(u'Required'))])
     is_eighteen = BooleanField(lazy_gettext(u'1VR_18'))
