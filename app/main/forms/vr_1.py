8f3431745eddea2c247a4a531742426641090311
Peter Karman <karpet@peknet.com>
2018-08-10 19:12:17 -0500

    Citizenship and Age questions use custom inline radio widget

--- app/main/forms/vr_1.py
+++ app/main/forms/vr_1.py
@@ -1,10 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, RadioField, widgets
+from wtforms.widgets import html_params
 from wtforms.validators import DataRequired
 from flask_babel import lazy_gettext
+from markupsafe import Markup
+
+class InlineListWidget(widgets.ListWidget):
+    def __call__(self, field, **kwargs):
+        self.html_tag = 'div'
+        kwargs.setdefault("id", field.id)
+        html = ["<%s %s>" % (self.html_tag, html_params(**kwargs))]
+        for subfield in field:
+            if self.prefix_label:
+                html.append("<div class='form-check form-check-inline'>%s %s</div>" % (subfield.label, subfield()))
+            else:
+                html.append("<div class='form-check form-check-inline'>%s %s</div>" % (subfield(), subfield.label))
+        html.append("</%s>" % self.html_tag)
+        return Markup("".join(html))
 
 class RadioBooleanField(RadioField):
-    widget = widgets.ListWidget()
+    widget = InlineListWidget()
     option_widget = widgets.RadioInput()
 
     def process_formdata(self, valuelist):
@@ -20,7 +35,11 @@ class FormVR1(FlaskForm):
     is_citizen = RadioBooleanField(
         lazy_gettext(u'1VR_citizen'),
         choices=[(False, lazy_gettext(u'choice_no')), (True, lazy_gettext(u'choice_yes'))],
-        default=False, # always require user action TODO broken
+        default='False', # always require user action
         validators=[DataRequired(message=lazy_gettext(u'Required'))]
     )
-    is_eighteen = BooleanField(lazy_gettext(u'1VR_18'))
+    is_eighteen = RadioBooleanField(
+        lazy_gettext(u'1VR_18'),
+        choices=[(False, lazy_gettext(u'choice_no')), (True, lazy_gettext(u'choice_yes'))],
+        default='False',
+    )
