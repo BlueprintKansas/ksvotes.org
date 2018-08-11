61dbd12064c025295399a7d2305e65a341beff6c
Peter Karman <karpet@peknet.com>
2018-08-10 19:45:01 -0500

    Citizenship and Age use Yes/No radio buttons instead of checkbox (#210)
    * Citizenship is Yes/No radio button group
    
    * create custom RadioBooleanField class to act like Boolean, look like Radio
    
    * port RadioBooleanField to render_field2
    
    * Citizenship and Age questions use custom inline radio widget
    
    * Fix client-side validation that citizenship == Yes

--- app/main/forms/vr_1.py
+++ app/main/forms/vr_1.py
@@ -1,8 +1,45 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField
+from wtforms import BooleanField, RadioField, widgets
+from wtforms.widgets import html_params
 from wtforms.validators import DataRequired
 from flask_babel import lazy_gettext
+from markupsafe import Markup
+
+class InlineListWidget(widgets.ListWidget):
+    def __call__(self, field, **kwargs):
+        self.html_tag = 'div'
+        kwargs.setdefault("id", field.id)
+        html = ["<%s %s>" % (self.html_tag, html_params(**kwargs))]
+        for subfield in field:
+            if self.prefix_label:
+                html.append("<div class='form-check form-check-inline'>%s %s</div>" % (subfield.label, subfield()))
+            else:
+                html.append("<div class='form-check form-check-inline'>%s %s</div>" % (subfield(), subfield.label))
+        html.append("</%s>" % self.html_tag)
+        return Markup("".join(html))
+
+class RadioBooleanField(RadioField):
+    widget = InlineListWidget()
+    option_widget = widgets.RadioInput()
+
+    def process_formdata(self, valuelist):
+        if 'False' in valuelist:
+            self.data = False
+        elif 'True' in valuelist:
+            self.data = True
+        else:
+            self.data = False
+
 
 class FormVR1(FlaskForm):
-    is_citizen = BooleanField(lazy_gettext(u'1VR_citizen'), validators=[DataRequired(message=lazy_gettext(u'Required'))])
-    is_eighteen = BooleanField(lazy_gettext(u'1VR_18'))
+    is_citizen = RadioBooleanField(
+        lazy_gettext(u'1VR_citizen'),
+        choices=[(False, lazy_gettext(u'choice_no')), (True, lazy_gettext(u'choice_yes'))],
+        default='False', # always require user action
+        validators=[DataRequired(message=lazy_gettext(u'Required'))]
+    )
+    is_eighteen = RadioBooleanField(
+        lazy_gettext(u'1VR_18'),
+        choices=[(False, lazy_gettext(u'choice_no')), (True, lazy_gettext(u'choice_yes'))],
+        default='False',
+    )
