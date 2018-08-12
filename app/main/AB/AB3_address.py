08bcef1f2b94f362c6095d65678a90f967b43ff5
Peter Karman <karpet@peknet.com>
2018-08-12 17:04:51 -0500

    Guess county based on ZIP5

--- app/main/AB/AB3_address.py
+++ app/main/AB/AB3_address.py
@@ -1,10 +1,11 @@
 from app.main import main
-from flask import g, url_for, render_template, redirect, request
+from flask import g, url_for, render_template, redirect, request, current_app
 from app.main.forms import FormAB3
 from app.decorators import InSession
 from app.services.steps import Step_AB_3
 from app import db
 from app.services import SessionManager
+from app.models import *
 
 @main.route('/ab/address', methods=["GET", "POST"])
 @InSession
@@ -29,6 +30,14 @@ def ab3_address():
             update_data['validated_addresses'] = step.validated_addresses
             g.registrant.update(update_data)
             g.registrant.addr_lookup_complete = step.addr_lookup_complete
+
+            # guess county based on ZIP if necessary.
+            if not g.registrant.county or len(g.registrant.county) == 0:
+                zip5 = g.registrant.try_value('zip')
+                county = ZIPCode.guess_county(zip5)
+                current_app.logger.info("Lookup county %s based on ZIP5 %s" %(county, zip5))
+                g.registrant.county = county
+
             db.session.commit()
             session_manager = SessionManager(g.registrant, step)
             return redirect(session_manager.get_redirect_url())
