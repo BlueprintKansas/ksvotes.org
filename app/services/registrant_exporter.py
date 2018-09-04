db1b72a1995003b87330a231b0c77ea8b62f81e0
Peter Karman <karpet@peknet.com>
2018-09-04 12:24:35 -0500

    Refactor to include all encrypted fields for all rows (#337)

--- app/services/registrant_exporter.py
+++ app/services/registrant_exporter.py
@@ -8,31 +8,26 @@ class RegistrantExporter():
         self.list_of_regs = list_of_regs
 
     def export(self):
-        # use the first item to create our header
-        r1 = self.list_of_regs[0]
-        fieldnames = list(dict(r1.__dict__).keys())
+        # must iterate over all rows to create the header,
+        # because not every row has all the same encrypted fields.
 
-        fieldnames.remove('_sa_instance_state') # internal key
-        fieldnames.remove('registration') # no need for encrypted blob
-
-        # append all the encrypted key names
-        # we use the r_ prefix just to namespace and avoid any collisions.
-        for k in r1.registration_value.keys():
-            if k == 'csrf_token':
-                continue
-            fieldnames.append('r_'+k)
-
-        writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
-        writer.writeheader()
-        
+        unique_field_names = {}
+        reg_dicts = []
         for r in self.list_of_regs:
             r_dict = dict(r.__dict__)
+            reg_dicts.append(r_dict)
             for k, v in r.registration_value.items():
                 if k == 'csrf_token':
                     continue
+                unique_field_names['r_'+k] = True
                 r_dict['r_'+k] = v
 
+        fieldnames = list(Registrant.__table__.columns.keys()) + list(unique_field_names.keys())
+        fieldnames.remove('registration') # no need for encrypted blob
+
+        writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
+        writer.writeheader()
+        for r_dict in reg_dicts:
             r_dict.pop('registration', None)
             r_dict.pop('_sa_instance_state', None)
-
             writer.writerow(r_dict)
