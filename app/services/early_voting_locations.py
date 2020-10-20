65bfb2b36d7b1e51f7fb6b0c632cb1da7c3e4627
Peter Karman <karpet@peknet.com>
2020-10-20 17:42:33 -0500

    service

---
+++ app/services/early_voting_locations.py
@@ -0,0 +1,41 @@
+from app.services.ksvotes_redis import KSVotesRedis
+from airtable import Airtable
+import os
+import json
+from flask import current_app
+
+class EarlyVotingLocations():
+  def __init__(self, county):
+    # disabled w/o this env var set
+    if not os.getenv('AIRTABLE_EV_TABLE'):
+      return
+
+    self.county = county
+
+    # look first in redis cache
+    self.locations = self.get_cached_locations()
+    #current_app.logger.info("redis locations for {}: {}".format(self.county, self.locations))
+
+    # otherwise fetch and cache
+    if not self.locations:
+      self.locations = self.fetch_locations()
+
+
+  def cache_key(self):
+    return "ev-{}".format(self.county)
+
+  def get_cached_locations(self):
+    redis = KSVotesRedis()
+    locations = redis.get(self.cache_key())
+    if locations:
+      return json.loads(locations)
+    else:
+      return None
+    
+  def fetch_locations(self):
+    redis = KSVotesRedis()
+    airtable = Airtable(os.getenv('AIRTABLE_EV_BASE_ID'), os.getenv('AIRTABLE_EV_TABLE'), os.getenv('AIRTABLE_EV_KEY'))
+    response = airtable.get_all(formula="AND( COUNTY = '{}' )".format(self.county.upper()))
+    redis.set(self.cache_key(), json.dumps(response).encode())
+    return response
+
