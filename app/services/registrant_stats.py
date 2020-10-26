05fbc59bb201293f88ee82ae03d4cd171e7954b9
Peter Karman <karpet@peknet.com>
2020-10-26 12:12:26 -0500

    Fix timezone offset in stats

--- app/services/registrant_stats.py
+++ app/services/registrant_stats.py
@@ -15,10 +15,10 @@ class RegistrantStats():
   def vr_through_today(self, start_date):
     today = datetime.date.today()
     sql = """
-      select cast(vr_completed_at as date), count(id)
+      select cast(vr_completed_at at time zone 'utc' at time zone 'america/chicago' as date), count(id)
       from registrants
-      where vr_completed_at is not null and vr_completed_at between '{start_date}' and '{today}'
-      group by cast(vr_completed_at as date)
+      where vr_completed_at is not null and vr_completed_at at time zone 'utc' at time zone 'america/chicago' between '{start_date}' and '{today}'
+      group by cast(vr_completed_at at time zone 'utc' at time zone 'america/chicago' as date)
       order by 1
     """
     vr_stats = db.session.connection().execute(sql.format(start_date=start_date, today=today))
@@ -28,10 +28,10 @@ class RegistrantStats():
   def ab_through_today(self, start_date):
     today = datetime.date.today()
     sql = """
-      select cast(ab_completed_at as date), count(id)
+      select cast(ab_completed_at at time zone 'utc' at time zone 'america/chicago' as date), count(id)
       from registrants
-      where ab_completed_at is not null and ab_completed_at between '{start_date}' and '{today}'
-      group by cast(ab_completed_at as date)
+      where ab_completed_at is not null and ab_completed_at at time zone 'utc' at time zone 'america/chicago' between '{start_date}' and '{today}'
+      group by cast(ab_completed_at at time zone 'utc' at time zone 'america/chicago' as date)
       order by 1
     """
     ab_stats = db.session.connection().execute(sql.format(start_date=start_date, today=today))
