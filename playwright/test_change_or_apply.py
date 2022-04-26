cbf3a69f2702d043ee395867df3148a64cac2f7e
Peter Karman <karpet@peknet.com>
2022-04-26 11:56:07 -0500

    increase CI log level

--- playwright/test_change_or_apply.py
+++ playwright/test_change_or_apply.py
@@ -2,6 +2,7 @@
 def complete_step_0(page):
     page.goto("/ref?ref=demo")
     page.locator("[name=email-confirm]").fill("nosuchperson@example.com")
+    assert page.input_value("[name=dob]") == "01/01/2000"
     page.locator("xpath=//button[@type='submit']").click()
     assert page.url.endswith("/change-or-apply/")
     assert page.locator("text=You are not registered").all_text_contents() == ["You are not registered to vote."]
