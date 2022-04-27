93594a46bdb7bc56f29f45d2b41993d2ba686615
Peter Karman <karpet@peknet.com>
2022-04-27 16:29:05 -0500

    Playwright tests pt 4 (#608)

--- playwright/test_vr_steps.py
+++ playwright/test_vr_steps.py
@@ -1,4 +1,4 @@
-from helpers import complete_step_0, click_submit, click_register_now
+from helpers import *
 
 def test_citizenship(page):
     complete_step_0(page)
@@ -10,3 +10,30 @@ def test_citizenship(page):
     page.locator("id=is_citizen-0").click() # only yes allowed to proceed
     click_submit(page)
     assert page.url.endswith("/vr/name")
+
+def test_name(page):
+    complete_step_0(page)
+    click_register_now(page)
+    page.locator("id=is_eighteen-0").click()
+    page.locator("id=is_citizen-0").click()
+    click_submit(page)
+    assert page.url.endswith("/vr/name")
+    # values are pre-filled from step0
+    assert page.input_value("[name=prefix]") == ""
+    assert page.input_value("[name=name_first]") == "No"
+    assert page.input_value("[name=name_middle]") == "Such"
+    assert page.input_value("[name=name_last]") == "Person"
+    assert page.input_value("[name=suffix]") == ""
+    # change middle name, then test it sticks
+    page.locator("[name=name_middle]").fill("Other")
+    click_submit(page)
+    click_back(page)
+    assert page.input_value("[name=name_middle]") == "Other"
+    # add name change
+    click_change_of_name(page)
+    page.select_option("select#prev_prefix", label="Miss")
+    page.locator("[name=prev_name_first]").fill("Previous No")
+    page.locator("[name=prev_name_middle]").fill("Previous Other")
+    page.locator("[name=prev_name_last]").fill("Previous Person")
+    page.select_option("select#prev_suffix", label="III")
+    click_submit(page)
