84957245bc1aacc2a94f5d272cd5b00c618011d2
Peter Karman <karpet@peknet.com>
2022-05-02 09:08:43 -0500

    AB playwright tests (#613)
    * AB playwright tests
    
    * clear signature before testing validation

---
+++ playwright/test_ab_steps.py
@@ -0,0 +1,37 @@
+from helpers import *
+
+def test_vr_to_ab(page):
+    complete_step_0(page)
+    click_register_now(page)
+    page.locator("id=is_eighteen-0").click()
+    page.locator("id=is_citizen-0").click()
+    click_submit(page) # step 1
+    click_submit(page) # step 2
+    click_submit(page) # step 3
+    click_submit(page) # step 4
+    click_submit(page) # step 5
+    create_signature(page)
+    click_sign(page) # step 6
+    select_county(page, "Douglas")
+    page.locator("[name=affirmation]").click()
+    click_submit(page)
+    assert page.url.endswith("/vr/submission")
+    assert len(page.locator("text=Success!").all_text_contents()) == 1
+    page.locator("text=Apply for Advance Ballot").click()
+    assert page.url.endswith("/ab/election_picker")
+    page.locator("id=elections-0").click()
+    click_submit(page)
+    assert page.url.endswith("/ab/identification")
+    # no value will trigger modal with instructions
+    click_submit(page)
+    page.locator("id=confirm-modal-try-again").click()
+    # any value is accepted
+    page.locator("id=ab_identification").fill("K00-00-0000")
+    click_submit(page)
+    assert page.url.endswith("/ab/preview")
+    select_county(page, "Douglas")
+    create_signature(page)
+    click_sign(page)
+    page.locator("id=confirm-modal-ok").click()
+    page.locator("[name=affirmation]").click()
+    click_submit(page)
