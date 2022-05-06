f37ad760f279f370825ada5b1484d1429b614789
Peter Karman <karpet@peknet.com>
2022-05-06 08:05:17 -0500

    fix playwright test for AB_PRIMARY_DEADLINE

--- playwright/test_ab_steps.py
+++ playwright/test_ab_steps.py
@@ -18,9 +18,15 @@ def test_vr_to_ab(page):
     assert page.url.endswith("/vr/submission")
     assert len(page.locator("text=Success!").all_text_contents()) == 1
     page.locator("text=Apply for Advance Ballot").click()
+
     assert page.url.endswith("/ab/election_picker")
-    page.locator("id=elections-0").click()
+    page.locator("id=elections-0").check() # with AB_PRIMARY_DEADLINE set, Primary election is an option to .check instead of .click
     click_submit(page)
+    assert page.url.endswith("/ab/election_picker")
+    assert len(page.locator("text=Required >> visible=true").all_text_contents()) == 2 # help text + Party validation error
+    page.select_option("select#party", label="Republican") # TODO in future this might optionally include Unaffiliated
+    click_submit(page)
+
     assert page.url.endswith("/ab/identification")
     # no value will trigger modal with instructions
     click_submit(page)
